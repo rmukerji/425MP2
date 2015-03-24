fd5a1c21a84ea678b3d6460da5a420e53afa010b
rmukerji <rmukerj2@illinois.edu>
2015-03-23 22:41:43 -0400

    initial setup done

--- node_class.py
+++ node_class.py
@@ -8,4 +8,30 @@ class node:
 		self.key_value = {}
 		self.finger_table = []
 
-	#define some other functions here	
+	def find_succesor(id):
+		#insert code here
+		a = 1
+
+	def find_predecessor(id):
+		#insert code here
+		a = 1
+
+	def closest_preceding_finger(id):
+		#insert code here
+		a = 1
+
+	def join(n_prime):
+		#insert code here
+		a = 1	
+
+	def init_finger_table(n_prime):
+		#insert code here
+		a = 1					
+	
+	def update_others():
+		#insert code here
+		a = 1
+
+	def update_finger_table(s, i):
+		#insert code here
+		a = 1	
